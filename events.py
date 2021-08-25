f21c6a6e58022537c881d2879a6d1e4d1f417e92
= <=>
2021-08-25 19:13:46 +1000

    dataclass event

--- events.py
+++ events.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
 class Event:
-    def __init__(self,event_type:str,info:str):
-        self.event_type = event_type
-        self.info = info
+    event_type: str
+    info: str
